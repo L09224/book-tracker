cf56b974c3a97d3b29b4c5c74da2c3fddaa49967
Leander Ots <leander.ots@gmail.com>
2024-08-11 18:23:08 +0000

    add basic My Books page

--- book_admin/views.py
+++ book_admin/views.py
@@ -8,18 +8,11 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 
-#temporarily edited out
-#class HomePage(TemplateView):
-#    """
-#    Displays home page"
-#    """
-#    template_name = 'index.html'
-
 # Create your views here.
 
 def home(request):
     return render(request, 'book_admin/home.html')
-    
+
 def add_book(request):
     if request.method == "POST":
         form = BookForm(request.POST)
@@ -34,9 +27,22 @@ def book_list(request):
     books = Book.objects.all()
     return render(request, 'book_admin/book_list.html', {'books': books})
 
+@login_required
 def user_book_list(request):
     user_books = UserBook.objects.filter(user=request.user)
-    return render(request, 'book_admin/user_book_list.html', {'user_books': user_books})
+
+    if request.method == 'POST':
+        book_id = request.POST.get('book_id')
+        user_book = UserBook.objects.get(id=book_id, user=request.user)
+        form = UserBookForm(request.POST, instance=user_book)
+        if form.is_valid():
+            form.save()
+            return redirect('user_book_list')
+    else:
+        form = UserBookForm()
+
+    return render(request, 'book_admin/user_book_list.html', {'user_books': user_books, 'form': form})
+
 
 def update_progress(request, pk):
     user_book = UserBook.objects.get(pk=pk)
@@ -62,13 +68,6 @@ def signup(request):
         form = UserCreationForm()
     return render(request, 'registration/signup.html', {'form': form})
 
-# Login required
-
-@login_required
-def user_book_list(request):
-    user_books = UserBook.objects.filter(user=request.user)
-    return render(request, 'book_admin/user_book_list.html', {'user_books': user_books})
-
 @login_required
 def update_progress(request, pk):
     user_book = UserBook.objects.get(pk=pk, user=request.user)
@@ -80,4 +79,20 @@ def update_progress(request, pk):
     else:
         form = UserBookForm(instance=user_book)
     return render(request, 'book_admin/update_progress.html', {'form': form})
+
+@login_required
+def my_books(request):
+    user_books = UserBook.objects.filter(user=request.user)
+
+    if request.method == 'POST':
+        book_id = request.POST.get('book_id')
+        user_book = UserBook.objects.get(id=book_id, user=request.user)
+        form = UserBookForm(request.POST, instance=user_book)
+        if form.is_valid():
+            form.save()
+            return redirect('user_book_list')
+    else:
+        form = UserBookForm()
+
+    return render(request, 'book_admin/my_books.html', {'user_books': user_books, 'form': form})  # Ensure this is correct
 
