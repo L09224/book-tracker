5990438f1e85ed6dcc5ce6675f25feac6fc1e450
Leander Ots <leander.ots@gmail.com>
2024-08-13 19:40:40 +0000

    ensure static files are being read

--- book_tracker/settings.py
+++ book_tracker/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['.gitpod.io', '.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -146,6 +146,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# WhiteNoise settings for serving static files in production
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
---
+++ staticfiles/js/book_admin.f07e98d3cc4f.js
@@ -0,0 +1,11 @@
+// Function to update (on /my-books page) the current page input to the total page number if the user updates teh status of their book to completed
+function handleFormSubmit(bookId, totalPages) {
+    const statusDropdown = document.getElementById('status_' + bookId);
+    const currentPageInput = document.getElementById('current_page_' + bookId);
+    
+    if (statusDropdown.value === 'completed') {
+        currentPageInput.value = totalPages;
+    }
+    
+    return true;
+}
